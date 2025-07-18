6001c3976d02f3cae90c77e2c4c782d1d1c72f81
manualemayla <manualemayla@gmail.com>
2025-07-18 11:55:23 +0200

    Create carousel.js

---
+++ carousel.js
@@ -0,0 +1,44 @@
+// carousel.js
+
+const images = [
+  'oasi_sketch_bw.jpg',
+  'oasi_green.jpg',
+  'oasi_winter_nav.jpg'
+];
+
+let currentImage = 0;
+let interval;
+
+const mainImage = document.getElementById('mainImage');
+const thumbnails = document.querySelectorAll('.thumbnail');
+
+function showImage(index) {
+  currentImage = index;
+  mainImage.src = images[index];
+  thumbnails.forEach((thumb, i) => {
+    thumb.classList.toggle('active', i === index);
+  });
+}
+
+function startSlideshow() {
+  interval = setInterval(() => {
+    currentImage = (currentImage + 1) % images.length;
+    showImage(currentImage);
+  }, 6000);
+}
+
+function stopSlideshow() {
+  clearInterval(interval);
+}
+
+thumbnails.forEach((thumb, index) => {
+  thumb.addEventListener('click', () => {
+    stopSlideshow();
+    showImage(index);
+    startSlideshow();
+  });
+});
+
+// Inizializzazione
+showImage(currentImage);
+startSlideshow();
